169a64ea77cf0ebe4a264f0e0f66eb723688a011
Samuel Kim <Samuel_Kim1@gmail.com>
2018-11-05 22:27:44 -0600

    Added SDL_Plotter and main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SDL_Plotter.h"
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    cout << "Hello world!" << endl;
+
+    SDL_Plotter p(500, 500);
+
+    return 0;
+}
