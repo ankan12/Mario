0bce044392c9f4ebf4c600889d95c7caa99da381
Samuel Kim <Samuel_Kim1@baylor.edu>
2018-11-14 15:40:25 -0600

    Update main

--- main.cpp
+++ main.cpp
@@ -12,16 +12,17 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-    const int screenWidth = 800, screenHeight = 800;
-
-    SDL_Plotter p(screenWidth, screenHeight);
+    const int screenWidth = 800, screenHeight = 400;
+    SDL_Plotter p(screenHeight, screenWidth);
 
     ifstream inFile;
 
     Sprite spawnPipeLeft("animatedPipe.txt", inFile);
+    spawnPipeLeft.set_y(50);
     spawnPipeLeft.setScale(2);
 
     Sprite spawnPipeRight("animatedPipe.txt", inFile);
+    spawnPipeRight.set_y(50);
     spawnPipeRight.setMirrored(true);
     spawnPipeRight.setScale(2);
     spawnPipeRight.set_x(screenWidth - spawnPipeRight.getWidth(0) *
@@ -29,13 +30,38 @@ int main(int argc, char ** argv)
 
     Sprite brickFloor("brickFloor.txt", inFile);
     brickFloor.setScale(2);
-    brickFloor.set_y(screenHeight - brickFloor.getHeight(0) *
-                           brickFloor.getScale());
+    brickFloor.set_y(screenHeight - brickFloor.getScaledHeight(0));
 
     Level level1;
-    level1.setBlockSprite("blueBlock.txt", inFile);
+    level1.setBlockSprite("blueBlock.txt", inFile, 2);
 
+    level1.placePlatform(0, spawnPipeLeft.get_y() +
+                         spawnPipeLeft.getScaledHeight(0), 25);
 
+    level1.placePlatform(800 - 25 * 14, level1.getPlatform(0).get_y(), 25);
+
+    cout << (200 -level1.getPlatform(0).get_y()) << endl;
+
+    level1.placePlatform(400 - 15 * 14, 200, 30);
+
+    level1.placePlatform(0, 290, 20);
+    level1.placePlatform(800 - 20 * 14, 290, 20);
+
+    level1.placePlatform(0, 210, 10);
+    level1.placePlatform(800 - 10 * 14, 210, 10);
+
+    Sprite bottomLeftPipe("bottomPipe.txt", inFile);
+    Sprite bottomRightPipe("bottomPipe.txt", inFile);
+
+    bottomLeftPipe.setScale(2);
+    bottomRightPipe.setScale(2);
+
+    bottomLeftPipe.setMirrored(true);
+
+    bottomLeftPipe.set_y(400 - brickFloor.getScaledHeight(0) -
+                         bottomLeftPipe.getScaledHeight(0));
+    bottomRightPipe.setLocation(800 - bottomRightPipe.getScaledWidth(0),
+                                bottomLeftPipe.get_y());
 
     while (!p.getQuit()){
 
@@ -53,7 +79,12 @@ int main(int argc, char ** argv)
         spawnPipeLeft.draw(p);
         spawnPipeRight.draw(p);
 
+        bottomLeftPipe.draw(p);
+        bottomRightPipe.draw(p);
+
         horizontalTile(brickFloor, 0, screenWidth, p);
+
+        level1.draw(p);
 
         p.update();
 
