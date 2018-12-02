fe877fa23156662a6796658ce6a03dcb536e0218
Miranda Montroy <MONTROY@ECS.BAYLOR.EDU>
2018-12-02 17:49:51 -0600

    added comments on cpp and h

--- Level.cpp
+++ Level.cpp
@@ -1,6 +1,14 @@
 #include "Level.h"
 #include <vector>
 
+
+/*
+ * description: constructor for Platform
+ * return: NA
+ * precondition: class instance exists
+ * postcondition: private variables are set to input
+ *
+*/
 Platform::Platform(int x, int y){
 
     this->x = x;
@@ -10,30 +18,65 @@ Platform::Platform(int x, int y){
 
 }
 
+/*
+ * description: gets x variable
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 int Platform::get_x(){
 
     return x;
 
 }
 
+/*
+ * description: gets y variable
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 int Platform::get_y(){
 
     return y;
 
 }
 
+/*
+ * description: gets and passes box by reference
+ * return: Block
+ * precondition: class instance exists and int passed in
+ * postcondition: block is passed by reference
+ *
+*/
 Block& Platform::getBlock(int index){
 
     return blocks[index];
 
 }
 
+/*
+ * description: gets the number of blocks
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 int Platform::numOfBlocks(){
 
     return blocks.size();
 
 }
 
+/*
+ * description: adds a block to save pixels into
+ * return: void
+ * precondition: class instance exists and header files are linked
+ * postcondition: sprite has been added
+ *
+*/
 void Platform::addBlock(Sprite& sprite){
 
     int width = sprite.getScaledWidth(0);
@@ -61,39 +104,88 @@ void Platform::addBlock(Sprite& sprite){
 
 }
 
+/*
+ * description: gets column locations by reference
+ * return: vector<int>
+ * precondition: class instance exists and header files linked
+ * postcondition: column locations passed by reference into vector<int>
+ *
+*/
 vector<int>& Platform::getColLocations(){
 
     return columnLocations;
 
 }
 
+/*
+ * description: gets collision box by reference
+ * return: CollisionBox
+ * precondition: class instance exists and header files linked
+ * postcondition: collision box passed by reference
+ *
+*/
 CollisionBox& Platform::getCollisionBox(){
 
     return cBox;
 
 }
 
+/*
+ * description: gets the number of columns
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 int Platform::numOfColumns(){
 
     return columnLocations.size();
 
 }
 
+/*
+ * description: gets the state of frozen
+ * return: bool
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 bool Platform::isFrozen(){
 
     return frozen;
 
 }
 
+/*
+ * description: sets the variable frozen
+ * return: void
+ * precondition: class instance exists and header files linked
+ * postcondition: frozen variable set
+ *
+*/
 void Platform::setFrozen(){
 
     frozen = true;
 
 }
 
+/*
+ * description: constructor for Level
+ * return: NA
+ * precondition: class instance exists
+ * postcondition: Nothing is changed
+ *
+*/
 Level::Level(){
 }
 
+/*
+ * description: draws input to the screen
+ * return: void
+ * precondition: class instance exists and SDL is passed in
+ * postcondition: input is drawn to the screen
+ *
+*/
 void Level::draw(SDL_Plotter& p){
 
     for (int w = 0; w < waveAnimations.size(); w++){
@@ -139,13 +231,26 @@ void Level::draw(SDL_Plotter& p){
     }
 }
 
-
+/*
+ * description: loads the sprite image
+ * return: void
+ * precondition: class instance exists
+ * postcondition: sprite is set and scaled
+ *
+*/
 void Level::setBlockSprite(char filename[], ifstream& inFile, int scale){
 
     blockSprite.loadImage(filename, inFile);
     blockSprite.setScale(scale);
 }
 
+/*
+ * description: loads the left edge sprite image
+ * return: void
+ * precondition: class instance exists
+ * postcondition: sprite is set and scaled
+ *
+*/
 void Level::setLeftEdgeSprite(char filename[], ifstream& inFile, int scale){
 
     leftEdgeSprite.loadImage(filename, inFile);
@@ -153,6 +258,13 @@ void Level::setLeftEdgeSprite(char filename[], ifstream& inFile, int scale){
 
 }
 
+/*
+ * description: loads the right edge sprite image
+ * return: void
+ * precondition: class instance exists
+ * postcondition: sprite is set and scaled
+ *
+*/
 void Level::setRightEdgeSprite(char filename[], ifstream& inFile, int scale){
 
     rightEdgeSprite.loadImage(filename, inFile);
@@ -160,6 +272,13 @@ void Level::setRightEdgeSprite(char filename[], ifstream& inFile, int scale){
 
 }
 
+/*
+ * description: used to tell if the file contains sprites
+ * return: bool
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 bool Level::containsEdgeSprites(){
 
     if (leftEdgeSprite.getTotalFrames() == 0 ||
@@ -173,12 +292,27 @@ bool Level::containsEdgeSprites(){
 
 }
 
+
+/*
+ * description: gets sprite by reference
+ * return: Sprite
+ * precondition: class instance exists
+ * postcondition: sprite is passed by reference
+ *
+*/
 Sprite& Level::getSprite(){
 
     return blockSprite;
 
 }
 
+/*
+ * description: block sprite is gotten and passed by reference
+ * return: Sprite
+ * precondition: class instance exists
+ * postcondition: sprite gotten by reference
+ *
+*/
 Sprite& Level::getSprite(BlockType type){
 
     switch (type){
@@ -196,6 +330,13 @@ Sprite& Level::getSprite(BlockType type){
 
 }
 
+/*
+ * description: sets the location for the platform
+ * return: void
+ * precondition: class instance exists
+ * postcondition: x, y, and number of blocks are set to input
+ *
+*/
 void Level::placePlatform(int x, int y, int numOfBlocks){
 
     Platform platform(x, y);
@@ -253,18 +394,39 @@ void Level::placePlatform(int x, int y, int numOfBlocks){
 
 }
 
+/*
+ * description: gets the number of platforms
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
 int Level::numOfPlatforms(){
 
     return platforms.size();
 
 }
 
+/*
+ * description: platform is gotten and passed by reference
+ * return: Platform
+ * precondition: class instance exists and int passed in
+ * postcondition: gets Platform by reference
+ *
+*/
 Platform& Level::getPlatform(int index){
 
     return platforms[index];
 
 }
 
+/*
+ * description: adds the wave animation
+ * return: void
+ * precondition: class instance exists and two ints are passed in
+ * postcondition: wave animation is added
+ *
+*/
 void Level::addWaveAnimation(int platNumber, int startX){
 
     WaveAnimation wa(platNumber, startX);
@@ -273,6 +435,13 @@ void Level::addWaveAnimation(int platNumber, int startX){
 
 }
 
+/*
+ * description: adds the freeze animation
+ * return: void
+ * precondition: class instance exists and two ints are passed in
+ * postcondition: freeze animation is added
+ *
+*/
 void Level::addFreezeAnimation(int platNumber, int startX){
 
 }
--- Level.h
+++ Level.h
@@ -21,26 +21,103 @@ private:
 
 public:
 
+/*
+ * description: constructor for Platform
+ * return: NA
+ * precondition: class instance exists
+ * postcondition: private variables are set to input
+ *
+*/
     Platform(int x, int y);
 
+/*
+ * description: gets and passes box by reference
+ * return: Block
+ * precondition: class instance exists and int passed in
+ * postcondition: block is passed by reference
+ *
+*/
     Block& getBlock(int index);
 
+/*
+ * description: gets the number of blocks
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int numOfBlocks();
 
+/*
+ * description: gets the number of columns
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int numOfColumns();
 
+/*
+ * description: gets x variable
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int get_x();
 
+/*
+ * description: gets y variable
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int get_y();
 
+/*
+ * description: adds a block to save pixels into
+ * return: void
+ * precondition: class instance exists and header files are linked
+ * postcondition: sprite has been added
+ *
+*/
     void addBlock(Sprite& sprite);
 
+/*
+ * description: gets column locations by reference
+ * return: vector<int>
+ * precondition: class instance exists and header files linked
+ * postcondition: column locations passed by reference into vector<int>
+ *
+*/
     vector<int>& getColLocations();
 
+/*
+ * description: gets collision box by reference
+ * return: CollisionBox
+ * precondition: class instance exists and header files linked
+ * postcondition: collision box passed by reference
+ *
+*/
     CollisionBox& getCollisionBox();
 
+/*
+ * description: gets the state of frozen
+ * return: bool
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     bool isFrozen();
 
+/*
+ * description: sets the variable frozen
+ * return: void
+ * precondition: class instance exists and header files linked
+ * postcondition: frozen variable set
+ *
+*/
     void setFrozen();
 
 };
@@ -56,26 +133,121 @@ private:
 
 public:
 
+/*
+ * description: constructor for Level
+ * return: NA
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     Level();
 
+/*
+ * description: draws input to the screen
+ * return: void
+ * precondition: class instance exists and SDL is passed in
+ * postcondition: input is drawn to the screen
+ *
+*/
     void draw(SDL_Plotter& p);
 
+/*
+ * description: sets the location for the platform
+ * return: void
+ * precondition: class instance exists
+ * postcondition: x, y, and number of blocks are set to input
+ *
+*/
     void placePlatform(int x, int y, int numOfBlocks);
 
+/*
+ * description: loads the sprite image
+ * return: void
+ * precondition: class instance exists
+ * postcondition: sprite is set and scaled
+ *
+*/
     void setBlockSprite(char[], ifstream&, int scale);
+
+/*
+ * description: loads the left edge sprite image
+ * return: void
+ * precondition: class instance exists
+ * postcondition: sprite is set and scaled
+ *
+*/
     void setLeftEdgeSprite(char[], ifstream&, int scale);
+
+/*
+ * description: loads the right edge sprite image
+ * return: void
+ * precondition: class instance exists
+ * postcondition: sprite is set and scaled
+ *
+*/
     void setRightEdgeSprite(char[], ifstream&, int scale);
 
+/*
+ * description: used to tell if the file contains sprites
+ * return: bool
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     bool containsEdgeSprites();
 
+/*
+ * description: gets sprite by reference
+ * return: Sprite
+ * precondition: class instance exists
+ * postcondition: sprite is passed by reference
+ *
+*/
     Sprite& getSprite();
+
+/*
+ * description: block sprite is gotten and passed by reference
+ * return: Sprite
+ * precondition: class instance exists
+ * postcondition: sprite gotten by reference
+ *
+*/
     Sprite& getSprite(BlockType type);
 
+/*
+ * description: gets the number of platforms
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int numOfPlatforms();
 
+/*
+ * description: platform is gotten and passed by reference
+ * return: Platform
+ * precondition: class instance exists and int passed in
+ * postcondition: gets Platform by reference
+ *
+*/
     Platform& getPlatform(int index);
 
+/*
+ * description: adds the wave animation
+ * return: void
+ * precondition: class instance exists and two ints are passed in
+ * postcondition: wave animation is added
+ *
+*/
     void addWaveAnimation(int platNumber, int startX);
+
+/*
+ * description: adds the freeze animation
+ * return: void
+ * precondition: class instance exists and two ints are passed in
+ * postcondition: freeze animation is added
+ *
+*/
     void addFreezeAnimation(int platNumber, int startX);
 
 };
