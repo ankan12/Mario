6e6ca4f94424386a1622aac4d21cec067d5fc965
SamuelKim1 <43525192+SamuelKim1@users.noreply.github.com>
2018-12-03 03:47:04 -0600

    "add comments to main"

--- main.cpp
+++ main.cpp
@@ -38,6 +38,8 @@ Thomas Cho
 #include "Music.h"
 using namespace std;
 
+/*This function causes a Level object to place 7 platforms in a symmetrical
+pattern*/
 void createLevel(Level& level);
 
 #include "Music.h"
@@ -47,6 +49,7 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
+    //Create the music objects
     Music die("mb_die.wav");
     Music coin("mb_coin.wav");
     Music touch("mb_touch.wav");
@@ -61,6 +64,7 @@ int main(int argc, char ** argv)
 
     ifstream inFile;
 
+    //Create the background
     Sprite spawnPipeLeft("animatedPipe.txt", inFile);
     spawnPipeLeft.set_y(50);
     spawnPipeLeft.setScale(2);
@@ -94,9 +98,10 @@ int main(int argc, char ** argv)
                                 bottomLeftPipe.get_y());
 
 
-
+    //Create the object that the player will control
     Plumber player("mario_sprite_sheet.txt", inFile, 2, 395, 150);
 
+    //Create collisionBoxes for the pipes
     CollisionBox cPipe1, cPipe2, cPipe3, cPipe4, cFloor;
     cPipe1.fitToSprite(spawnPipeLeft);
     cPipe2.fitToSprite(spawnPipeRight);
@@ -108,11 +113,13 @@ int main(int argc, char ** argv)
 
     CollisionBox cCeiling(800,5,0,0);
 
+    //Vector of CollisionBoxes that contains all of the solids in the game
     vector<CollisionBox> solids;
 
     solids.push_back(cFloor);
     solids.push_back(cCeiling);
 
+    //pipe0 and pipe1 are used for transporting the enemies
     Pipe pipe0;
     pipe0.direction = "right";
     pipe0.entrance.resetAtLocation(cPipe4.get_x() - 2, cPipe4.get_y());
@@ -133,14 +140,15 @@ int main(int argc, char ** argv)
     pipe1.exitX = cPipe2.get_x();
     pipe1.exitY = cPipe2.get_y();
 
-
+    //Add all of the platforms to the vector of solids
     for (int i = 0; i < level1.numOfPlatforms(); i++){
         solids.push_back(level1.getPlatform(i).getCollisionBox());
     }
 
+    //This is used for controlling the amount of time between enemy spawns
     int enemySpawnTimer = 0;
-    int enemies = 0;
 
+    //Create a sprite and collision box for the pow block
     Sprite pow("pow.txt", inFile);
     pow.setScale(2);
     pow.setLocation(400-16, 250);
@@ -148,10 +156,12 @@ int main(int argc, char ** argv)
     powBox.fitToSprite(pow);
     powBox.type = "pow";
 
+    //Sprite used for drawing Mario's lives
     Sprite life("life.txt",inFile);
     life.setScale(2);
     int lives = 10;
 
+    //Create a Number object that displays the score
     int score = 0;
     Number scoreDisplay("digits.txt", inFile);
     scoreDisplay.setLocation(100, 0);
@@ -160,25 +170,32 @@ int main(int argc, char ** argv)
     center.setScale(2);
     center.setLocation(350,150);
 
+    //Create a TextBox object that displays when the next phase occurs.
     TextBox topTextBox("letters.txt",inFile);
     topTextBox.setScale(2);
     topTextBox.setLocation(350, 50);
 
+    //Boolean values to let the program know when the next phase or level occurs.
     bool nextPhase = true;
     bool nextLevel = true;
 
+    //When this is true, the program stops spawning enemies.
     bool waitForPhaseToEnd = false;
 
+    //Integer values that hold the current level and phase.
     int level = 0;
     int phase = 0;
 
-    bool startScreen = true;
-
+    //Object that reads a file that contains a list of enemies
     EnemySpawner enemySpawner;
+
+    //Object that controls every aspect of every enemy
     Enemies allEnemies(pipe0, pipe1);
 
+    //Object that deals with scores
     Score scoreKeeper;
 
+    //Draw the introductory screen
     drawBlackBackground(800, 400, p);
     Sprite logo("marioBrosLogo.txt",inFile);
     logo.setScale(2);
@@ -190,6 +207,7 @@ int main(int argc, char ** argv)
     xToContinue.setScale(2);
     xToContinue.draw(p);
     p.update();
+    //Wait until the player presses x
     while (p.getKey() != 'X'){
         p.kbhit();
         if (p.getQuit()){
@@ -197,11 +215,13 @@ int main(int argc, char ** argv)
         }
     }
 
+    //The loop of the game
     while (!p.getQuit()){
 
+        //This code is executed during a level transition
         if (nextLevel){
             level++;
-
+            //Set the visuals of the level to correspond with the level number
             switch(level){
             case 1:
                 level1.setBlockSprite("blueBlock.txt",inFile,2);
@@ -235,6 +255,7 @@ int main(int argc, char ** argv)
                 break;
             default:
 
+                //End screen, for when level is greater than 5
                 drawBlackBackground(800,400,p);
                 ostringstream writer;
                 writer << "Thanks for playing!\nYour score is ";
@@ -242,30 +263,34 @@ int main(int argc, char ** argv)
 
                 scoreKeeper.storeScores();
 
+                //Set the top score
                 if (score > scoreKeeper.getScore(0)){
                     scoreKeeper.setScore(score, 0);
                 }
 
                 scoreKeeper.storeScores();
 
+                //Display the top score
                 writer << "\nTop score is " << scoreKeeper.getScore(0);
                 center.setLocation(100,200);
                 center.setText(writer.str());
                 center.draw(p);
                 p.update();
+                //Wait for the player to exit the program
                 while (!p.getQuit()){
                     p.kbhit();
-                    if (p.getQuit()){
-                        return 1;
-                    }
                 }
             }
 
+            //Exit the program after the end screen
             if (level > 5){
-                break;
+                return 1;
             }
 
-            phase = 1;
+
+            //Code that is executed at the start of every level
+
+            //Show the player that a new level is starting
             p.clear();
             drawBlackBackground(800, 400, p);
             ostringstream writer;
@@ -275,12 +300,13 @@ int main(int argc, char ** argv)
             center.draw(p);
             p.update();
             p.Sleep(1000);
+
+            //Reset everything about the level
             nextLevel = false;
             lives = 10;
             pow.setCurrentFrame(0);
             enemySpawnTimer = 0;
             allEnemies.clearEnemies();
-            enemies = 0;
             player.placeCharacterInCenter();
             solids.clear();
             solids.push_back(powBox);
@@ -296,19 +322,24 @@ int main(int argc, char ** argv)
 
         }
 
+        //This code is executed every frame
+
+        //Spawn an enemy every 150 frames
         if (enemySpawnTimer == 150 && !waitForPhaseToEnd){
 
             waitForPhaseToEnd = allEnemies.addEnemy(enemySpawner.getNextEnemy(), inFile);
             enemySpawnTimer = 0;
-            enemies++;
         }
 
         enemySpawnTimer++;
 
+        //Update the player's location
         player.updateLocation();
 
+        //This code executes if a player hits his head under a pow box.
         if (player.getCBox().hitHeadUnder(powBox) && pow.getCurrentFrame() < 3){
 
+            //Create random waves on the platforms
             for (int t = 0; t < 300; t++){
 
                 int randPlat = rand()%level1.numOfPlatforms();
@@ -317,42 +348,54 @@ int main(int argc, char ** argv)
 
             }
 
+            //bump all of the enemies
             for (int ID = 0; ID < allEnemies.getEnemyCBoxes().size(); ID++){
                 if (allEnemies.stateOfEnemy(ID) == grounded || allEnemies.stateOfEnemy(ID) == bumpedAndGrounded || allEnemies.stateOfEnemy(ID) == aliveAndFalling){
                     allEnemies.bumpEnemy(ID);
                 }
             }
 
+            //Pause the screen for a short amount of time
             p.Sleep(10);
+
+            //Shorten the pow block every time it is used
             pow.nextFrame();
             if (pow.getCurrentFrame() == 3){
                 removeCollisionBox(solids, "pow");
             }
         }
 
+        /*Check if the player has touched any solid objects, and prevent the player
+        from passing through them.*/
         player.solidCollisions(solids, level1);
+
+        //Check the player's collisions with enemies
         if (!player.getDead()){
             for (int i = 0; i < allEnemies.getEnemyCBoxes().size(); i++){
 
+                //Create reference variables with shorter names
                 CollisionBox& hurtBox = allEnemies.getEnemyHurtBoxes()[i];
                 CollisionBox& hitBox = allEnemies.getEnemyCBoxes()[i];
                 CollisionBox& c = player.getCBox();
 
+                //This code executes if the player's fist hits the enemy's hurtBox, which is below the platform.
                 if (player.getFist().hitHeadUnder(hurtBox) && (allEnemies.stateOfEnemy(hurtBox.ID) == grounded || allEnemies.stateOfEnemy(hurtBox.ID) == bumpedAndGrounded)){
                     allEnemies.bumpEnemy(hurtBox.ID);
                 }
 
-
+                //This code executes if the player touches the enemy.
                 if (c.isTouching(hitBox)){
 
                     touch.playSound();
 
+                    //If the enemy has been bumped and is on the ground, kick the enemy down and increase your score.
                     if (allEnemies.stateOfEnemy(hitBox.ID) == bumpedAndGrounded){
                         allEnemies.kickEnemyIfBumped(hitBox.ID, player.getXVel()/2.0);
                         score += 800;
 
                     }
 
+                    //If the enemy has not been bumped, take one life away from the player.
                     else if ( !player.getInvincible() && (allEnemies.stateOfEnemy(hitBox.ID) == grounded || allEnemies.stateOfEnemy(hitBox.ID) == aliveAndFalling) ){
                         player.setYVel(-2);
                         player.setXVel(0);
@@ -362,8 +405,10 @@ int main(int argc, char ** argv)
                         lives--;
                         die.playSound();
 
+                        //This codes executes if the player loses all of their lives.
                         if (lives == 0){
 
+                            //Draw the game over screen
                             drawBlackBackground(800,400,p);
                             TextBox gameOver("letters.txt",inFile);
                             gameOver.setLocation(350,50);
@@ -388,6 +433,8 @@ int main(int argc, char ** argv)
                                     return 1;
                                 }
                             }
+
+                            //Show that the player's score gets divided in half when they lose all of their lives.
                             int originalScore = score;
                             score /= 2;
                             for (int c = originalScore; c >= score; c-=(originalScore-score)/100 ){
@@ -425,6 +472,8 @@ int main(int argc, char ** argv)
             }
         }
 
+        //Check if there are zero enemies, and if all of the enemies in the phase have been spawned already.
+        //If that is the case, set nextPhase to true.
         if (allEnemies.getEnemyCBoxes().size() == 0 && waitForPhaseToEnd){
             waitForPhaseToEnd = false;
             nextPhase = true;
@@ -436,15 +485,16 @@ int main(int argc, char ** argv)
             }
         }
 
+        //Update the locations of all of the enemies.
         allEnemies.updateLocations();
+
+        //Update a vector of collision boxes for all of the enemies
         allEnemies.updateCollisionBoxes();
 
-//        for (int i = 0; i < allEnemies.getEnemyCBoxes().size(); i++){
-//            solids.push_back(allEnemies.getEnemyCBoxes()[i]);
-//        }
-
+        //Make sure that none of the enemies pass through any solids
         allEnemies.solidCollisions(solids);
 
+        //Add freeze animations where any slip ices have self-destructed
         for (int i = 0; i < allEnemies.getSlipices().size(); i++){
 
             Slipice& slip = allEnemies.getSlipices()[i];
@@ -459,8 +509,6 @@ int main(int argc, char ** argv)
         }
 
 
-//        removeCollisionBox(solids, "enemy");
-
         // Draw Black
         drawBlackBackground(screenWidth, screenHeight, p);
 
@@ -468,34 +516,52 @@ int main(int argc, char ** argv)
         p.kbhit();
         key_pressed = p.getKey();
         player.onKeyPress(key_pressed);
+
+        //This is a back door to be able to see the other levels easily.
         if (key_pressed == ' '){
             nextLevel = true;
         }
+
+        //This is a back door to be able to easily increase your score.
+        //This resets the pow block, which normally resets at the beginning of
+        //each level.
         if (key_pressed == 'P'){
             removeCollisionBox(solids,"pow");
             solids.push_back(powBox);
             pow.setCurrentFrame(0);
         }
 
+        //Draw the brick floor
         horizontalTile(brickFloor, 0, screenWidth, p);
 
+        //Draw each enemy
         allEnemies.draw(p);
 
+        //Draw the two upper pipes.
         spawnPipeLeft.draw(p);
         spawnPipeRight.draw(p);
 
+        //Draw the two lower pipes.
         bottomLeftPipe.draw(p);
         bottomRightPipe.draw(p);
 
+        //Draw all of the platforms, which are contained in a Level class.
         level1.draw(p);
+
+        //Draw the pow block
         pow.draw(p);
 
+        //Draw the player
         player.draw(p);
 
+        //Draw the player's lives in the upper left corner
         drawLives(life, 0, 0, lives, p);
+
+        //Update and draw the player's score at the top of the screen
         scoreDisplay.setNumber(score);
         scoreDisplay.draw(p);
 
+        //Indicate to the player when a new phase has begun
         if (nextPhase){
             ostringstream writer;
             writer << "Phase " << phase;
@@ -506,8 +572,10 @@ int main(int argc, char ** argv)
             }
         }
 
+        //Update the plotter
         p.update();
 
+        //Pause the game for 2 milliseconds
         p.Sleep(2);
 
     }
@@ -515,6 +583,8 @@ int main(int argc, char ** argv)
     return 0;
 }
 
+/*This function causes a Level object to place 7 platforms in a symmetrical
+pattern*/
 void createLevel(Level& level){
 
     level.clearPlatforms();
