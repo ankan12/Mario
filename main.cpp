2a4222cc70230d9bcde521eea4679822f2a07e19
ankan12 <anupamakannan3210@gmail.com>
2018-12-02 20:34:22 -0600

    Altered main to include music files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <fstream>
 #include "SDL_Plotter.h"
@@ -16,12 +14,18 @@
 #include <cmath>
 #include "Plumber.h"
 #include <ctime>
+#include "Music.h"
 
 using namespace std;
 
-
 int main(int argc, char ** argv)
 {
+    Music die("mb_die.wav");
+    Music coin("mb_coin.wav");
+    Music touch("mb_touch.wav");
+    bgMusic bg("bg_music.wav", 1000);
+
+    bg.playMusic();
 
     char key_pressed;
 
@@ -53,6 +57,7 @@ int main(int argc, char ** argv)
 
     level1.placePlatform(800 - 25 * 14, level1.getPlatform(0).get_y(), 25);
 
+    cout << (200 -level1.getPlatform(0).get_y()) << endl;
 
     level1.placePlatform(400 - 15 * 14, 200, 30);
 
@@ -208,14 +213,16 @@ int main(int argc, char ** argv)
                         creepers[i].setXVel(player.getXVel()/2.0);
                         creepers[i].setYAccel(0.2);
                         creepers[i].setState(deadAndFalling);
-
+                        touch.playSound();
                     }
                     if ( !player.getInvincible() && (creepers[i].getState() == grounded || creepers[i].getState() == aliveAndFalling) ){
                         player.setYVel(-2);
                         player.setXVel(0);
+                        //player.setYAccel(2);
                         player.setFalling(true);
                         player.getSprite().setCurrentFrame(6);
                         player.setDead(true);
+                        die.playSound();
                     }
 
                 }
@@ -230,6 +237,7 @@ int main(int argc, char ** argv)
             }
             creepers[i].updateLocation2();
             creepers[i].solidCollisions2(solids);
+            //creepers[i].solidCollisions2(creepBoxes);
             if (creepers[i].getState() == grounded){
                 for (int i = 0; i < creepBoxes.size(); i++){
                     CollisionBox& b = creepBoxes[i];
@@ -258,7 +266,12 @@ int main(int argc, char ** argv)
         horizontalTile(brickFloor, 0, screenWidth, p);
 
         for (int i = 0; i < creepers.size(); i++){
-        creepers[i].draw2(p);
+//            if (!(creepers[i].getDead() && creepers[i].getY() > 350)){
+//                creepers[i].draw(p);
+//            }
+            creepers[i].draw2(p);
+//            creepers[i].getCBox().drawBox(p);
+//            creepers[i].getHitBox().drawBox(p);
 
         }
 
@@ -276,13 +289,253 @@ int main(int argc, char ** argv)
         pipe0.entrance.drawBox(p);
         pipe1.entrance.drawBox(p);
 
+
+//        plotSquare(pipe0.entranceX, pipe0.entranceY, 2, 0, 0, 255, p);
+//        plotSquare(pipe0.exitX, pipe0.exitY, 2, 0, 255, 0, p);
+//        plotSquare(pip0.entranceX, pip0.entranceY, 2, 0, 0, 255, b);
+//        plotSquare(pip0.entranceX, pip0.entranceY, 2, 0, 0, 255, b);
+
         // Update Screen
         p.update();
 
-        p.Sleep(10);
+        p.Sleep(2);
 
     }
 
     return 0;
 }
 
+//
+//#include <iostream>
+//#include <fstream>
+//#include "SDL_Plotter.h"
+//#include "Sprite.h"
+//#include "CollisionBox.h"
+//#include "Wave.h"
+//#include "Number.h"
+//#include "Video.h"
+//#include "Level.h"
+//#include "DrawingFunctions.h"
+//#include "WaveAnimation.h"
+//#include <cstdlib>
+//#include "TextBox.h"
+//#include "FreezeAnimation.h"
+//using namespace std;
+//
+//
+//int main(int argc, char ** argv)
+//{
+//    cout << "Hello!" << endl;
+//
+//    const int screenWidth = 800, screenHeight = 400;
+//    SDL_Plotter p(screenHeight, screenWidth);
+//
+//    ifstream inFile;
+//
+//    Sprite spawnPipeLeft("animatedPipe.txt", inFile);
+//    spawnPipeLeft.set_y(50);
+//    spawnPipeLeft.setScale(2);
+//
+//    Sprite spawnPipeRight("animatedPipe.txt", inFile);
+//    spawnPipeRight.set_y(50);
+//    spawnPipeRight.setMirrored(true);
+//    spawnPipeRight.setScale(2);
+//    spawnPipeRight.set_x(screenWidth - spawnPipeRight.getWidth(0) *
+//                         spawnPipeRight.getScale());
+//
+//    Sprite brickFloor("brickFloor.txt", inFile);
+//    brickFloor.setScale(2);
+//    brickFloor.set_y(screenHeight - brickFloor.getScaledHeight(0));
+//
+//    Level level1;
+//    level1.setBlockSprite("blueBlock.txt", inFile, 2);
+//
+//    level1.placePlatform(0, spawnPipeLeft.get_y() +
+//                         spawnPipeLeft.getScaledHeight(0), 25);
+//
+//    level1.placePlatform(800 - 25 * 14, level1.getPlatform(0).get_y(), 25);
+//
+//    cout << (200 -level1.getPlatform(0).get_y()) << endl;
+//
+//    level1.placePlatform(400 - 15 * 14, 200, 30);
+//
+//    level1.placePlatform(0, 290, 20);
+//    level1.placePlatform(800 - 20 * 14, 290, 20);
+//
+//    level1.placePlatform(0, 210, 10);
+//    level1.placePlatform(800 - 10 * 14, 210, 10);
+//
+//    Sprite bottomLeftPipe("bottomPipe.txt", inFile);
+//    Sprite bottomRightPipe("bottomPipe.txt", inFile);
+//
+//    bottomLeftPipe.setScale(2);
+//    bottomRightPipe.setScale(2);
+//
+//    bottomLeftPipe.setMirrored(true);
+//
+//    bottomLeftPipe.set_y(400 - brickFloor.getScaledHeight(0) -
+//                         bottomLeftPipe.getScaledHeight(0));
+//    bottomRightPipe.setLocation(800 - bottomRightPipe.getScaledWidth(0),
+//                                bottomLeftPipe.get_y());
+//
+//    WaveAnimation wa(0, 50);
+//    //wa.setSpeed_1sthalf(0.2);
+//    //wa.setSpeed_2ndhalf(0.2);
+//
+//    vector<WaveAnimation> wAnimations;
+//
+////    CollisionBox cBox(100, 100, 750, 0);
+////    cBox.setHorizontalWrap(800);
+//
+//    TextBox tBox("letters.txt", inFile);
+//
+//    tBox.setLocation(10, 0);
+//    tBox.setScale(3);
+//
+//    tBox.setText("ABCDEFG HIJKLMNOPQRSTUVWXYZ\n0123456789!!!!");
+//
+//    Number num("digits.txt", inFile);
+//    num.setScale(3);
+//    num.setLocation(700, 0);
+//    num.setNumber(100);
+//
+//    Sprite mario("mario_sprite_sheet.txt", inFile);
+//
+//    mario.set_x(400);
+//    mario.set_y(323);
+//    mario.setScale(2);
+//
+//    Sprite coin("coin.txt", inFile);
+//    Sprite coin2("coin.txt", inFile);
+//    coin2.setLocation(0, 50);
+//
+//    vector<FreezeAnimation> fAnimations;
+//
+//    while (!p.getQuit()){
+//
+//        if (p.kbhit()){
+//            switch(p.getKey()){
+//            case ' ':{
+//
+//                int p = rand() % level1.numOfPlatforms();
+//
+//                int col = rand() % level1.getPlatform(p).getColLocations().size();
+//
+//                WaveAnimation wa(p, col);
+//
+//                wa.setDistance(30);
+//
+//                //wa.setWaveWidth(20.0/3.14159);
+//
+//                wa.setSpeed_2ndhalf(0.1);
+//
+//                //wa.setHeight(16.0);
+//
+//                wAnimations.push_back(wa);
+//
+//                mario.nextFrame();
+//                break;
+//            }
+//            case 'F':
+//
+//                FreezeAnimation f("leftIce.txt","middleIce.txt","rightIce.txt",
+//                                  inFile, 2, 5);
+//                int p = rand() % level1.numOfPlatforms();
+//                f.setPlatformNumber(p);
+//
+//                fAnimations.push_back(f);
+//
+//                break;
+//
+////            case 'W':
+////                tBox.setText("w");
+////                break;
+////
+////            case 'D':
+////                tBox.setText("d");
+////                break;
+////
+////            case 'A':
+////                tBox.setText("a");
+////                break;
+//
+//            }
+//        }
+//
+//        p.clear();
+//
+//        drawBlackBackground(screenWidth, screenHeight, p);
+//
+//        spawnPipeLeft.draw(p);
+//        spawnPipeRight.draw(p);
+//
+//        bottomLeftPipe.draw(p);
+//        bottomRightPipe.draw(p);
+//
+//        horizontalTile(brickFloor, 0, screenWidth, p);
+//
+//        wa.setNextFrame(level1.getPlatform(0).getColLocations());
+//
+//        for (int w = 0; w < wAnimations.size(); w++){
+//
+//            WaveAnimation& wa = wAnimations[w];
+//
+//            if (wa.finished()){
+//                wAnimations.erase(wAnimations.begin() + w);
+//                continue;
+//            }
+//
+//            Platform& p = level1.getPlatform(wa.platformNum());
+//
+//            wa.setNextFrame(p.getColLocations());
+//
+//        }
+//
+//        for (int i = 0; i < fAnimations.size(); i++){
+//
+//
+//            if (fAnimations[i].finished()){
+//                fAnimations.erase(fAnimations.begin() + i);
+//                continue;
+//            }
+//
+//            int p = fAnimations[i].getPlatformNumber();
+//
+//            fAnimations[i].setNextFrame(level1.getPlatform(p));
+//
+//        }
+//
+//        level1.draw(p);
+//
+//
+//        for (int i = 0; i < level1.numOfPlatforms(); i++){
+//
+//            level1.getPlatform(i).getCollisionBox().drawBox(p);
+//
+//        }
+//
+////        cBox.drawBox(p);
+//
+//        num.setNumber(wAnimations.size());
+//
+//        tBox.draw(p);
+//        num.draw(p);
+//
+//        mario.draw(p);
+//
+//        coin.nextFrame(20);
+//        coin2.nextFrame(10);
+//
+//        coin2.draw(p);
+//
+//        coin.draw(p);
+//
+//        p.update();
+//
+//        p.Sleep(1);
+//
+//    }
+//
+//    return 0;
+//}
+//>>>>>>> Stashed changes
