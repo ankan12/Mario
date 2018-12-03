62b6e44cb1cb428b88750463f131c84e8f811233
Miranda Montroy <MONTROY@ECS.BAYLOR.EDU>
2018-12-02 18:59:33 -0600

    added comments from the h

--- DrawingFunctions.cpp
+++ DrawingFunctions.cpp
@@ -1,5 +1,13 @@
 #include "DrawingFunctions.h"
 #include "Sprite.h"
+
+/*
+ * description: draws a square
+ * return: void
+ * precondition: six ints and a plotter are passed in
+ * postcondition: square drawn to screen
+ *
+*/
 void plotSquare(int x, int y, int scale, int r, int g, int b, SDL_Plotter& p){
 
     for (int c = 0; c < scale; c++){
@@ -11,6 +19,13 @@ void plotSquare(int x, int y, int scale, int r, int g, int b, SDL_Plotter& p){
     }
 }
 
+/*
+ * description: draws the black background
+ * return: void
+ * precondition: two ints and a plotter passed in
+ * postcondition: black background drawn to screen
+ *
+*/
 void drawBlackBackground(int screenWidth, int screenHeight, SDL_Plotter& p){
 
     for (int y = 0; y < screenHeight; y++){
@@ -25,6 +40,13 @@ void drawBlackBackground(int screenWidth, int screenHeight, SDL_Plotter& p){
 
 }
 
+/*
+ * description: draws horizontal tiles
+ * return: void
+ * precondition: passed in sprite, two ints, and a plotter
+ * postcondition: tiles drawn to screen
+ *
+*/
 void horizontalTile(Sprite& sprite, int startX, int endX, SDL_Plotter& p){
 
     int cf = sprite.getCurrentFrame();
