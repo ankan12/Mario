5ed9631d54faabaf2f2b54595252ee590caca618
Miranda Montroy <MONTROY@ECS.BAYLOR.EDU>
2018-12-02 18:57:57 -0600

    added comments to h and cpp

--- Number.h
+++ Number.h
@@ -14,7 +14,7 @@ private:
     vector<int> digits;
     int scale;
 
-public: //what do these do?
+public:
 
 /*
  * description: constructor for Number
@@ -26,22 +26,66 @@ public: //what do these do?
     Number(char[], ifstream&);
 
 /*
- * description: constructor for Number
+ * description: sets and scales number
  * return: void
- * precondition: instance of Number
- * postcondition: variables are set to input
+ * precondition: instance of Number and int is passed in
+ * postcondition: number is resized and set
  *
 */
     void setNumber(int);
+
+/*
+ * description: returns number
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int getNumber();
 
+/*
+ * description: sets location to output number
+ * return: void
+ * precondition: class instance exists and two ints are passed
+ * postcondition: x and y are set for location
+ *
+*/
     void setLocation(int, int);
 
+/*
+ * description: uses SDL to draw to the screen
+ * return: void
+ * precondition: class instance exists and plotter is passed in
+ * postcondition: nothing is changed, input drawn to screen
+ *
+*/
     void draw(SDL_Plotter& p);
 
+/*
+ * description: gets sprite and passes it by reference
+ * return: Sprite
+ * precondition: class instance exists
+ * postcondition: sprite is gotten and passed by reference
+ *
+*/
     Sprite& getSprite();
 
+/*
+ * description: sets the scale variable =
+ * return: void
+ * precondition: class instance exists and int is passed in
+ * postcondition: scale variable is set
+ *
+*/
     void setScale(int);
+
+/*
+ * description: gets the scale variable
+ * return: int
+ * precondition: class instance exists
+ * postcondition: nothing is changed
+ *
+*/
     int getScale();
 
 };
